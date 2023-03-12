b73829ee09a3546d35171f054b1135f2569ff52d
MORVWY <89845489+MORVWY@users.noreply.github.com>
2023-03-12 15:39:35 +0200

    fixed hamburger and nav

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,31 +1,22 @@
 function onHamb() {
-    const hamb = document.querySelector('.hamb'),
-    nav = document.querySelector('.nav-menu');
+  const hamb = document.querySelector(".hamb"),
+    nav = document.querySelector(".navbar-list");
 
-    hamb.addEventListener('click', (e) => {
+  hamb.addEventListener("click", () => {
+    hamb.classList.toggle("active");
+    nav.classList.toggle("activeNav");
 
-        hamb.classList.toggle('active');
-        nav.classList.toggle('activeNav');
+    document.body.classList.toggle("overflowHidden");
+  });
 
-        checkOverflow();
+  document.querySelectorAll(".list-item__link").forEach((item) => {
+    item.addEventListener("click", () => {
+      hamb.classList.remove("active");
+      nav.classList.remove("activeNav");
+
+      document.body.classList.remove("overflowHidden");
     });
-
-    document.querySelectorAll('.nav-menu__item').forEach(item => {
-        item.addEventListener('click', () => {
-            hamb.classList.remove('active');
-            nav.classList.remove('activeNav');
-
-            checkOverflow();
-        });
-    });
-
-    function checkOverflow() {
-        if (document.body.classList.contains('overflowHidden')) {
-            document.body.classList.remove('overflowHidden');
-        } else {
-            document.body.classList.add('overflowHidden');
-        }
-    }
+  });
 }
 
-export {onHamb};
+export { onHamb };
